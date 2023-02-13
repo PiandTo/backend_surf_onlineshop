f19ba43aaeea5d5a03b17f25d6d53a9e30dd1907
snaomi <mikhail.malev@gmail.com>
2023-02-13 19:47:02 +0300

    correct_image

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO backendshop.product (id, name, type, price, new_product, hot_product, old_price, image, description)
-    VALUES (1, 'shorts', 'NEW', 100, 'false', 'true', 150, 'images/s1.jpg', 'Old');
+    VALUES (1, 'shorts', 'NEW', 100, 'false', 'true', 150, 's1.jpg', 'Old');
